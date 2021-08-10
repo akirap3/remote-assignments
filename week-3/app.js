6d769dee5620f699b6a875aab444536a0f5848c9
akirap3 <akirapf3@gmail.com>
2021-08-10 17:19:03 +0800

    Change route sequence of routes to fix getData api route mismatch

--- week-3/app.js
+++ week-3/app.js
@@ -19,9 +19,9 @@ app.use("/", express.static("public"));
 
 // if there is route mismatch, please adjusty the sequence of routes
 app.use(router);
+app.use("/getData", getDataRoutes);
 app.use("/trackName", trackNameRoutes);
 app.use("/", indexRoutes);
-app.use("/getData", getDataRoutes);
 
 app.use((req, res, next) => {
   const err = new Error("Not found");
