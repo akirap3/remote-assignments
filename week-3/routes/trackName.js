62bd186a2baf385b41d00fc90ec165744d43145d
akirap3 <akirapf3@gmail.com>
2021-08-11 23:54:53 +0800

    delete get method on /tackName, redirect get method on /trackName to /:name

--- week-3/routes/trackName.js
+++ week-3/routes/trackName.js
@@ -1,27 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  const username = req.cookies.username;
-  const queryname = req.query.name;
-  if (username) {
-    if (username === queryname) {
-      res.render("trackName", { username });
-    } else {
-      res.render("trackName", { queryname, username });
-    }
-  } else {
-    res.render("index");
-  }
-});
-
-router.post("/", (req, res) => {
+router.all("/", (req, res) => {
   const { username } = req.body;
   if (username) {
     res.cookie("username", username);
     res.render("trackName", { username });
   } else {
-    res.render("index");
+    const queryname = req.query.name;
+    if (queryname === undefined) {
+      res.redirect("/");
+    } else {
+      res.redirect(`/${queryname}`);
+    }
   }
 });
 
