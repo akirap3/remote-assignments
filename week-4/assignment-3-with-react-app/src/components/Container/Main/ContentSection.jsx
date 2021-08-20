47ff0fa6dc98e3300097758a7ad07e3fd2f1ce21
akirap3 <akirapf3@gmail.com>
2021-08-20 21:23:31 +0800

    using react-uuid to create unique keys for elements

--- week-4/assignment-3-with-react-app/src/components/Container/Main/ContentSection.jsx
+++ week-4/assignment-3-with-react-app/src/components/Container/Main/ContentSection.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import uuid from "react-uuid";
 
 const ContentSection = (props) => {
   const arr = Array(4)
     .fill()
     .map((x, i) => i + 1);
   const content = arr.map((number) => (
-    <div key={Math.random() * 100 + number} className="content">
+    <div key={uuid()} className="content">
       <h3>Content Box {number}</h3>
     </div>
   ));
